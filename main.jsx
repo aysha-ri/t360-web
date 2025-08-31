edfaf1fc230d5996a321d9e086a7e08c5d8ea69e
aysha-ri <65112604+aysha-ri@users.noreply.github.com>
2025-08-31 15:58:31 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,4 +2,5 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
+// Mount the React app at the #root element
 createRoot(document.getElementById("root")).render(<App />);
