03fcfec3336fbf5aff6d1443bfa6204e1d213922
aysha-ri <65112604+aysha-ri@users.noreply.github.com>
2025-08-31 15:51:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Vite configuration for React + ES Modules
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-  },
+    host: "0.0.0.0"
+  }
 });
